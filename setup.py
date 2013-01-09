9638bb6c0d64547fd5c8c5bc89ee974f9a5adf0d
Francesco Banconi <frankban@gmail.com>
2013-01-09 19:25:51 +0100

    Restored URL in setup.py.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(name='django-generic-ratings',
     description='Django ratings tools supporting ajax, generic content type scores, multiple ratings for each content object.',
     author='Francesco Banconi',
     author_email='francesco.banconi@gmail.com',
+    url='https://bitbucket.org/frankban/django-generic-ratings/',
     zip_safe=False,
     packages=[
         'ratings',
